0daf87da184cbbc68d51755a2b871413890d8864
sairam098 <manikandank.mgp.be@gmail.com>
2022-03-27 18:47:48 +0530

    automation

---
+++ src/main/java/com/prog/App.java
@@ -0,0 +1,13 @@
+package com.prog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
